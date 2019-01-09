67ce1f4ae1b710fa834ae93f024cf2348c66cbf9
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2019-01-09 01:16:56 -0800

    Added Installer.py file

---
+++ installer.py
@@ -0,0 +1,21 @@
+__author__ = 'cartologic'
+
+info = {
+    "title": "Dashboard 2",
+    "description": "Create awesome dashboards using maps and other charts",
+    "author": 'Cartologic',
+    "home_page": 'http://cartoview.org/apps/cartoview_dashboard_2',
+    "help_url": "http://cartoview.org/apps/cartoview_dashboard/help/_2",
+    "tags": ['app', 'map', 'dashboard'],
+    "licence": 'BSD',
+    "author_website": "http://www.cartoview.org",
+    "single_instance": False
+}
+
+
+def install():
+    pass
+
+
+def uninstall():
+    pass
