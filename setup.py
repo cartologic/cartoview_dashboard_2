f0eb9a432a8b7893fa2ec7253354a271bd78a709
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2019-01-09 01:24:08 -0800

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from setuptools import find_packages
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+setup(
+  name = 'cartoview_dashboard_2',
+  packages = find_packages(),
+  version = '0.1',
+  description = 'Create awesome dashboards using maps and other charts',
+  long_description=long_description,
+  author = 'Cartologic',
+  author_email = 'info@cartologic.com',
+  url = 'https://github.com/cartologic/cartoview_dashboard_2',
+  include_package_data=True,
+  keywords = ['cartoview', 'gis', 'geonode', "django", "web mapping", "applications", "apps", "dashboard"],
+  classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Framework :: Django :: 1.8",
+    "Topic :: Scientific/Engineering :: GIS"
+  ],
+  license="BSD",
+  install_requires= ['cartoview']
+)
