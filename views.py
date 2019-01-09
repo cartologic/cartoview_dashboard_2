671b37fa25355a4da38d3f0164b7bf8c58d2206f
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2019-01-09 01:26:44 -0800

    Added Default View to app

--- views.py
+++ views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePage(TemplateView):
+    def get(self, request, *args, **kwargs):
+        return render(request, "react_dashboard/build/index.html", {})
