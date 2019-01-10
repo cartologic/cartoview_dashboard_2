d6c83764be8051e7f1db9e89f4ba1331bbd6fe9b
Unknown <ibrahim.a.hamid@gmail.com>
2019-01-10 17:39:58 +0200

    Dashboard Model created and registered to admin

--- models.py
+++ models.py
@@ -3,3 +3,11 @@ from django.db import models
 
 class Dashboard(models.Model):
     title = models.CharField(max_length=120)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
