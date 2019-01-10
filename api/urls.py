e6130d8c76d2b9ce30d32c23b5e39cad6779d0e9
Unknown <ibrahim.a.hamid@gmail.com>
2019-01-10 22:41:06 +0200

    API has its own urls and views now

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import DashboardListAPI
+from .views import DashboardListAPI, DashboardDetailAPI
 
 
 urlpatterns = [
-    url(r'^dashboards/', DashboardListAPI.as_view()),
+    url(r'^dashboards/(?P<id>\d+)/$', DashboardDetailAPI.as_view(), name="DashboardAPIDetail"),
+    url(r'^dashboards/$', DashboardListAPI.as_view(), name="DashboardAPIList"),
 ]
 
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from ..models.Dashboard import Dashboard
 from .serializers import DashboardSerializer
 
@@ -6,3 +6,9 @@ from .serializers import DashboardSerializer
 class DashboardListAPI(ListAPIView):
     queryset = Dashboard.objects.all()
     serializer_class = DashboardSerializer
+
+
+class DashboardDetailAPI(RetrieveAPIView):
+    queryset = Dashboard.objects.all()
+    serializer_class = DashboardSerializer
+    lookup_field = 'id'
