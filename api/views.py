658cfe3eee8716742ba61717d7f9329ec0439c28
Unknown <ibrahim.a.hamid@gmail.com>
2019-01-11 11:12:40 +0200

    List view now supports pagination

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsOwner
+from .pagination import DashboardPageNumberPagination
 from ..models.Dashboard import Dashboard
 from .serializers import DashboardGeneralSerializer, DashboardCreateSerializer
 
@@ -8,6 +9,7 @@ from .serializers import DashboardGeneralSerializer, DashboardCreateSerializer
 class DashboardListAPI(ListAPIView):
     queryset = Dashboard.objects.all()
     serializer_class = DashboardGeneralSerializer
+    pagination_class = DashboardPageNumberPagination
 
 
 class DashboardDetailAPI(RetrieveAPIView):
